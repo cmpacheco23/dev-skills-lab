4fa24614793b695a4d197728f4b288fb3c1bbc63
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-09-12 17:09:54 -0400

    import skills data to controller file and controller to route skills file

--- routes/skills.js
+++ routes/skills.js
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 
-import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controllers/skills.js'
+
 const router = Router()
 
 // GET localhost:3000/todos
