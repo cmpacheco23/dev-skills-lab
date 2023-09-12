fa3bdf4716b5d51248c55549eedf5e767a6373fb
cmpacheco23 <cmpachecomuevecela@gmail.com>
2023-09-12 17:00:44 -0400

    rename to skills and rewrite data to match

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'JavaScript', isFavorite: true, proficiency: 'Advanced'},
+  {text: 'React', isFavorite: false, proficiency: 'Beginner'},
+  {text: 'Node', isFavorite: true, proficiency: 'Intermediate'}
+]
+
+export { 
+	skills
+}
